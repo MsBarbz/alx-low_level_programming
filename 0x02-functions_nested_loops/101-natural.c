92711e25c7925c36732c03af90a27b157b8312f2
Barbra <barbrakachembere@gmail.com>
2023-07-19 20:02:02 -0700

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -3,7 +3,7 @@
 
 /**
  * main - Lists all the natural numbers below 1024 (excluded)
- * that are multiples of 3 or 5.
+ *        that are multiples of 3 or 5.
  *
  * Return: Always 0.
  */
@@ -11,16 +11,13 @@ int main(void)
 {
 	int i, sum = 0;
 
-
 	for (i = 0; i < 1024; i++)
 	{
 		if ((i % 3) == 0 || (i % 5) == 0)
 			sum += i;
 	}
 
-
 	printf("%d\n", sum);
-
 
 	return (0);
 }
